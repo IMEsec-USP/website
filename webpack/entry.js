ac5dde22502e28fb35ae9673bc22f085228d477d
Andrew Ijano <andrew.lopes@uol.com.br>
2020-03-21 18:52:04 -0300

    Removes animation and changes layout

--- webpack/entry.js
+++
@@ -1,2 +0,0 @@
-import "./animateSplash";
-import "./scrambleLogo";
--- webpack/scrambleLogo.js
+++
@@ -1,9 +0,0 @@
-import lottie from "lottie-web";
-
-lottie.loadAnimation({
-  container: document.getElementById("splash--logo"),
-  renderer: "svg",
-  loop: false,
-  autoplay: true,
-  path: "/assets/json/IMEsec-logo-animation.json"
-});
